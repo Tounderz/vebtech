ee6aca3ad672db1250e78f56e3269651bf70c0be
Tounderz <85989034+Tounderz@users.noreply.github.com>
2023-10-12 14:11:15 +0300

    Update Role.cs

--- vebtech.Domain/Models/Role.cs
+++ vebtech.Domain/Models/Role.cs
@@ -5,5 +5,5 @@ public class Role
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int UserId { get; set; }
-    public User? User { get; set; }
+    public User User { get; set; }
 }
